77d961dbee5063fa4bc51744a71ae35fbf2b67c9
jose <joseesapel@gmail.com>
2025-11-11 19:53:22 -0500

    feat: configuraciones

--- src/main/java/auth/sistema/config/Config.java
+++ src/main/java/auth/sistema/config/Config.java
@@ -2,25 +2,37 @@ package auth.sistema.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class Config {
     // aqui desabilitamos toda la seguridad de sprint para poder hacer peticiones sin permiso
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
-            );
-        
+    SecurityFilterChain  web(HttpSecurity http) throws Exception {
+        http.cors(withDefaults())
+                .csrf(csrf-> csrf.disable())
+                .authorizeHttpRequests((authorize)   -> authorize
+                        .requestMatchers("api/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                
+                
+                
+                )
+                .sessionManagement((session)  -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                
+                
+                ) ;
         return http.build();
+                
+            
+    
     }
     // aqui creamos la configuracion de las contraseñas encriptadas
     @Bean
